c5195de27acebe4e2370d7b09090dadd64a3029f
Leonardo Muñoz <leonardoeleazar.mv@gmail.com>
2025-07-05 03:22:57 -0400

    fix toogle

--- src/utils/getIconFromCdn.ts
+++ src/utils/getIconFromCdn.ts
@@ -1,40 +1,42 @@
 export const getIcon = (language: string) => {
   const icon = language.toLowerCase().replace(' ', '').replace('api', ''); // replace spaces and word api with empty string
+  const colorIcon = "/white"
+  
   if (icon === 'express') {
-    return 'https://cdn.simpleicons.org/express/white';
+    return `https://cdn.simpleicons.org/express${colorIcon}`;
   }
   if (icon === 'socketio') {
-    return 'https://cdn.simpleicons.org/socketio/white';
+    return `https://cdn.simpleicons.org/socketio${colorIcon}`;
   }
   if (icon === 'openai') {
-    return 'https://cdn.simpleicons.org/openai/white';
+    return `https://cdn.simpleicons.org/openai${colorIcon}`;
   }
   if (icon === 'ollama') {
-    return 'https://cdn.simpleicons.org/ollama/white';
+    return `https://cdn.simpleicons.org/ollama${colorIcon}`;
   }
   if (icon === 'whatsapp-web.js') {
-    return 'https://cdn.simpleicons.org/whatsapp';
+    return `https://cdn.simpleicons.org/whatsapp${colorIcon}`;
   }
   if (icon === 'mysql') {
-    return 'https://cdn.simpleicons.org/mysql/white';
+    return `https://cdn.simpleicons.org/mysql${colorIcon}`;
   }
   if (icon === 'postgresql') {
-    return 'https://cdn.simpleicons.org/postgresql/white';
+    return `https://cdn.simpleicons.org/postgresql${colorIcon}`;
   }
   if (icon === 'sequelize') {
-    return 'https://cdn.simpleicons.org/sequelize/white';
+    return `https://cdn.simpleicons.org/sequelize${colorIcon}`;
   }
   if (icon === 'quasar') {
-    return 'https://cdn.simpleicons.org/quasar/white';
+    return `https://cdn.simpleicons.org/quasar${colorIcon}`;
   }
   if (icon === 'electron') {
-    return 'https://cdn.simpleicons.org/electron/white';
+    return `https://cdn.simpleicons.org/electron${colorIcon}`;
   }
   if (icon === 'vuex4') {
-    return 'https://cdn.simpleicons.org/shopee/white';
+    return `https://cdn.simpleicons.org/shopee${colorIcon}`;
   }
   if (icon === 'handlebars.js') {
-    return 'https://cdn.simpleicons.org/handlebars.js/white';
+    return `https://cdn.simpleicons.org/handlebars.js${colorIcon}`;
   }
   return `https://cdn.simpleicons.org/${icon}`;
 };
