30f85fe8768b3bc9480c24a771dac614c51a3a56
Leonardo Muñoz <leonardoeleazar.mv@gmail.com>
2025-07-10 13:13:37 -0400

    feat: CNAME

--- astro.config.ts
+++ astro.config.ts
@@ -6,7 +6,7 @@ import vue from "@astrojs/vue";
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), vue()],
-    site: "https://your-domain.com", // Replace with your actual domain
+    site: "https://portafolio.pctester.cl", // Replace with your actual domain
     build: {
         assets: "assets",
         
