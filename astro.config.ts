1b0565cb865dedd641adb31df8f9bb12058010f4
Leonardo Muñoz <leonardoeleazar.mv@gmail.com>
2025-07-12 20:35:29 -0400

    fix: mejoras visuales

--- astro.config.ts
+++ astro.config.ts
@@ -3,9 +3,11 @@ import tailwind from "@astrojs/tailwind";
 
 import vue from "@astrojs/vue";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), vue()],
+    integrations: [tailwind(), vue(), icon()],
     site: "https://portafolio.pctester.cl", // Replace with your actual domain
     build: {
         assets: "assets",
